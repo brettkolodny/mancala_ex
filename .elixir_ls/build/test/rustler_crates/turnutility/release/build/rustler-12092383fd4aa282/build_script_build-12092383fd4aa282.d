d6e2f891d694d8da94fafc8bc7d8755ce23f66f6
Brett Kolodny <brettkolodny@gmail.com>
2020-09-17 16:26:19 -0400

    Updated gitignore

--- .elixir_ls/build/test/rustler_crates/turnutility/release/build/rustler-12092383fd4aa282/build_script_build-12092383fd4aa282.d
+++
@@ -1,5 +0,0 @@
-/home/brett/workspace/projects/mancala_ex/.elixir_ls/build/test/rustler_crates/turnutility/release/build/rustler-12092383fd4aa282/build_script_build-12092383fd4aa282: /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/rustler-0.22.0-rc.0/build.rs
-
-/home/brett/workspace/projects/mancala_ex/.elixir_ls/build/test/rustler_crates/turnutility/release/build/rustler-12092383fd4aa282/build_script_build-12092383fd4aa282.d: /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/rustler-0.22.0-rc.0/build.rs
-
-/home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/rustler-0.22.0-rc.0/build.rs:
--- .elixir_ls/build/test/rustler_crates/turnutility/release/deps/unicode_xid-a61258ecaf1d30f2.d
+++
@@ -1,8 +0,0 @@
-/home/brett/workspace/projects/mancala_ex/.elixir_ls/build/test/rustler_crates/turnutility/release/deps/unicode_xid-a61258ecaf1d30f2.rmeta: /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/brett/workspace/projects/mancala_ex/.elixir_ls/build/test/rustler_crates/turnutility/release/deps/libunicode_xid-a61258ecaf1d30f2.rlib: /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/brett/workspace/projects/mancala_ex/.elixir_ls/build/test/rustler_crates/turnutility/release/deps/unicode_xid-a61258ecaf1d30f2.d: /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs:
-/home/brett/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs:
